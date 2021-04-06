2da2af32f4271f0f476d06251b7e939ea6f2ee30
tomfarrell-yfw <81968950+tomfarrell-yfw@users.noreply.github.com>
2021-04-05 20:21:59 -0400

    systemupdate

---
+++ systemupdate.ps1
@@ -0,0 +1,9 @@
+Test-NetConnection -ComputerName ftp.sophos.com -Port 990 -InformationLevel Quiet
+Test-NetConnection -ComputerName ftp.sophos.com -Port 990 -InformationLevel Quiet
+Test-NetConnection -ComputerName ftp.sophos.com -Port 990 -InformationLevel Quiet
+
+
+
+
+
+
